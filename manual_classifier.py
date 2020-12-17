b66778942c66bb877d3c1b9f38072cc050897f0b
SamGalanakis <47306720+SamGalanakis@users.noreply.github.com>
2020-12-17 23:58:08 +0200

    manual classifier basically working!

---
+++ manual_classifier.py
@@ -0,0 +1,58 @@
+import os
+import pandas as pd
+import numpy as np
+from utils import (
+load_las,
+compare_clouds,
+extract_area,
+random_subsample,
+compare_clouds
+
+)
+
+
+def manual_classifier(
+    point_list_df,
+    file_1,
+    file_2,
+    class_labels,
+    sample_size=2048,
+    clearance=5,
+    shape='cylinder',
+        ):
+    points1 = load_las(file_1)
+    points2 = load_las(file_2)
+    change_classification_list = []
+    centers = []
+    for index, row in point_list_df.iterrows():
+        center = np.array([row['x'],row['y']])
+        extraction_1 = random_subsample(extract_area(points1,center,clearance,shape),sample_size)
+        extraction_2 = random_subsample(extract_area(points2,center,clearance,shape),sample_size)
+        change_classification = compare_clouds(extraction_1,extraction_2,class_labels)
+        change_classification_list.append(change_classification)
+    point_list_df['clearance'] = clearance
+    point_list_df['classification'] = change_classification_list
+    return point_list_df
+
+if __name__ == "__main__":
+    dir_1 = "D:/data/cycloData/2016/"
+    dir_2 = "D:/data/cycloData/2020/"
+    class_labels = ['nochange','removed','change']
+    point_list_dir = "D:/data/cycloData/point_lists/2016-2020/"
+    classified_dir = "D:/data/cycloData/point_lists_classified/2016-2020/"
+    point_list_files = [os.path.join(point_list_dir,f) for f in os.listdir(point_list_dir) if os.path.isfile(os.path.join(point_list_dir, f))]
+
+    point_list_dfs = [pd.read_csv(x,header=None) for x in point_list_files]
+    for x in point_list_dfs:
+        x.columns = ['name',"x","y","z"]
+    files_dir_1 = [os.path.join(dir_1,f) for f in os.listdir(dir_1) if os.path.isfile(os.path.join(dir_1, f)) and f.split(".")[-1]=='las']
+    files_dir_2 = [os.path.join(dir_2,f) for f in os.listdir(dir_2) if os.path.isfile(os.path.join(dir_2, f))and f.split(".")[-1]=='las']
+    files_dir_1 = sorted(files_dir_1,key=lambda x: int(os.path.basename(x).split("_")[0]))
+    files_dir_2 = sorted(files_dir_2,key=lambda x: int(os.path.basename(x).split("_")[0]))
+    
+    for point_list_df, file_1, file_2 in zip(point_list_dfs,files_dir_1,files_dir_2):
+        point_list_df_returned = manual_classifier(point_list_df,file_1,file_2,clearance=5,class_labels=class_labels)
+
+
+
+
--- utils.py
+++ utils.py
@@ -4,8 +4,18 @@ import numpy as np
 import time
 import pylas 
 from laspy.file import File
+from pyntcloud import PyntCloud
+import pandas as pd
 
-
+from matplotlib import widgets
+from mpl_toolkits import mplot3d
+import matplotlib as mpl
+import matplotlib.colors as colors
+import matplotlib.patches as patches
+import matplotlib.mathtext as mathtext
+import matplotlib.pyplot as plt
+import matplotlib.artist as artist
+import matplotlib.image as image
 #Losses from original repo
 def loss_fun(z, z_ldetJ, prior_z, e, e_ldetJ, prior_e):
     ll_z = prior_z.log_prob(z.cpu()).to(z.device) + z_ldetJ
@@ -39,7 +49,46 @@ def load_las(path):
     return points
 
 
+def compare_clouds(extraction_1,extraction_2,class_labels):
+    rgb1 =     np.rint(np.divide(extraction_1[:,3:],extraction_1[:,3:].max(axis=0))*255).astype(np.uint8)
+    rgb2 =     np.rint(np.divide(extraction_2[:,3:],extraction_2[:,3:].max(axis=0))*255).astype(np.uint8)
+    
+    # class_to_return = None
+    points1 = extraction_1[:,:3]
+    
+    points2 = extraction_2[:,:3]
+    points2[:,0]+=5
+    axes = plt.axes(projection='3d')
+    axes.scatter(points1[:,0], points1[:,1], points1[:,2], c = rgb1/255, s=1)
 
+    axes.scatter(points2[:,0], points2[:,1], points2[:,2], c = rgb2/255, s=1)
+    plt.axis('off')
+    
+    props = ItemProperties(labelcolor='black', bgcolor='yellow',
+                        fontsize=10, alpha=0.2)
+    hoverprops = ItemProperties(labelcolor='white', bgcolor='blue',
+                                fontsize=10, alpha=0.2)
+
+    menuitems = []
+
+    def on_select(item):
+        global class_to_return
+        class_to_return = item.labelstr
+        plt.close()
+        print('you selected %s' % item.labelstr)
+    for label in class_labels:
+      
+        item = MenuItem(plt.gcf(), label, props=props, hoverprops=hoverprops,
+                        on_select=on_select)
+        menuitems.append(item)
+
+    menu = Menu(plt.gcf(), menuitems)
+    plt.show()
+    return class_to_return
+
+
+    
+    
 
 def extract_area(full_cloud,center,clearance,shape= 'cylinder'):
     if shape == 'square':
@@ -120,12 +169,166 @@ def view_cloud(points,rgb_array=False,subsample=False):
 
 
     show(points, __doc__, axes=True)
+
+
+class ItemProperties:
+    def __init__(self, fontsize=14, labelcolor='black', bgcolor='yellow',
+                 alpha=1.0):
+        self.fontsize = fontsize
+        self.labelcolor = labelcolor
+        self.bgcolor = bgcolor
+        self.alpha = alpha
+
+        self.labelcolor_rgb = colors.to_rgba(labelcolor)[:3]
+        self.bgcolor_rgb = colors.to_rgba(bgcolor)[:3]
+
+
+class MenuItem(artist.Artist):
+    parser = mathtext.MathTextParser("Bitmap")
+    padx = 5
+    pady = 5
+
+    def __init__(self, fig, labelstr, props=None, hoverprops=None,
+                 on_select=None):
+        artist.Artist.__init__(self)
+
+        self.set_figure(fig)
+        self.labelstr = labelstr
+
+        if props is None:
+            props = ItemProperties()
+
+        if hoverprops is None:
+            hoverprops = ItemProperties()
+
+        self.props = props
+        self.hoverprops = hoverprops
+
+        self.on_select = on_select
+
+        x, self.depth = self.parser.to_mask(
+            labelstr, fontsize=props.fontsize, dpi=fig.dpi)
+
+        if props.fontsize != hoverprops.fontsize:
+            raise NotImplementedError(
+                'support for different font sizes not implemented')
+
+        self.labelwidth = x.shape[1]
+        self.labelheight = x.shape[0]
+
+        self.labelArray = np.zeros((x.shape[0], x.shape[1], 4))
+        self.labelArray[:, :, -1] = x/255.
+
+        self.label = image.FigureImage(fig, origin='upper')
+        self.label.set_array(self.labelArray)
+
+        # we'll update these later
+        self.rect = patches.Rectangle((0, 0), 1, 1)
+
+        self.set_hover_props(False)
+
+        fig.canvas.mpl_connect('button_release_event', self.check_select)
+
+    def check_select(self, event):
+        over, junk = self.rect.contains(event)
+        if not over:
+            return
+
+        if self.on_select is not None:
+            self.on_select(self)
+
+    def set_extent(self, x, y, w, h):
+        
+        self.rect.set_x(x)
+        self.rect.set_y(y)
+        self.rect.set_width(w)
+        self.rect.set_height(h)
+
+        self.label.ox = x + self.padx
+        self.label.oy = y - self.depth + self.pady/2.
+
+        self.hover = False
+
+    def draw(self, renderer):
+        self.rect.draw(renderer)
+        self.label.draw(renderer)
+
+    def set_hover_props(self, b):
+        if b:
+            props = self.hoverprops
+        else:
+            props = self.props
+
+        r, g, b = props.labelcolor_rgb
+        self.labelArray[:, :, 0] = r
+        self.labelArray[:, :, 1] = g
+        self.labelArray[:, :, 2] = b
+        self.label.set_array(self.labelArray)
+        self.rect.set(facecolor=props.bgcolor, alpha=props.alpha)
+
+    def set_hover(self, event):
+        """
+        Update the hover status of event and return whether it was changed.
+        """
+        b, junk = self.rect.contains(event)
+
+        changed = (b != self.hover)
+
+        if changed:
+            self.set_hover_props(b)
+
+        self.hover = b
+        return changed
+
+
+class Menu:
+    def __init__(self, fig, menuitems):
+        self.figure = fig
+        fig.suppressComposite = True
+
+        self.menuitems = menuitems
+        self.numitems = len(menuitems)
+
+        maxw = max(item.labelwidth for item in menuitems)
+        maxh = max(item.labelheight for item in menuitems)
+
+        x0 = 100
+        y0 = 400
+
+        width = maxw + 2*MenuItem.padx
+        height = maxh + MenuItem.pady
+
+        for item in menuitems:
+            left = x0
+            bottom = y0 - maxh - MenuItem.pady
+
+            item.set_extent(left, bottom, width, height)
+
+            fig.artists.append(item)
+            y0 -= maxh + MenuItem.pady
+
+        fig.canvas.mpl_connect('motion_notify_event', self.on_move)
+
+    def on_move(self, event):
+        draw = False
+        for item in self.menuitems:
+            draw = item.set_hover(event)
+            if draw:
+                self.figure.canvas.draw()
+                break
+
+
+
 if __name__ == "__main__":
     points = load_las("D:/data/cycloData/2016/0_5D4KVPBP.las")
     #grid_split(points,5000)
     #view_cloud(points[:,:3],points[:,3:],subsample = False)
     #view_cloud(points[:,:3],points[:,3:],subsample = False)
     #view_cloud(points[:,:3],points[:,3:],subsample = False)
+    
     sign_point = np.array([86967.46,439138.8])
     points = extract_area(points,sign_point,1.5,'cylinder')
+    points = random_subsample(points,3000)
+    class_return = compare_clouds(points[:,0:3],points[:,3:],['change','nochange'])
+    print(class_return)
     view_cloud(points[:,:3],points[:,3:],subsample = False)
