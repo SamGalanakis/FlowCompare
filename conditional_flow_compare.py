fix normalization and preprocesing and add eps

conditional_flow_compare.py
@@ -69,8 +69,6 @@ def main():
         extract_0 = torch.cat(extract_0)
         extract_1 = torch.stack(extract_1)
         batch_id_0 = torch.cat(batch_id_0)
-        if (extract_0.isnan().any() or extract_1.isnan().any()).item():
-            print()
         return [extract_0, batch_id_0, extract_1]
 
   
@@ -83,9 +81,8 @@ def main():
     one_up_path = os.path.dirname(__file__)
     out_path = os.path.join(one_up_path,"save/processed_dataset")
     dataset=ConditionalDataGrid(dirs,out_path=out_path,preload=preload,subsample=subsample,sample_size=sample_size,min_points=min_points)
- 
-    for x in range(len(dataset)):
-        dataset[x]
+    dataset[51]
+    
     
     shuffle=True
     dataloader = DataLoader(dataset,shuffle=shuffle,batch_size=batch_size,num_workers=num_workers,collate_fn=my_collate,pin_memory=True,prefetch_factor=2)

dataloaders/ConditionalDataGrid.py
@@ -15,7 +15,7 @@ eps = 1e-8
 
 
 class ConditionalDataGrid(Dataset):
-    def __init__(self, direcories_list,out_path,sample_size=2000,grid_square_size = 4,clearance = 28,preload=False,min_points=500,subsample='random'):
+    def __init__(self, direcories_list,out_path,sample_size=2000,grid_square_size = 4,clearance = 28,preload=False,min_points=500,subsample='random',height_min_dif=0.5,normalization='min_max'):
         self.sample_size  = sample_size
         self.grid_square_size = grid_square_size
         self.clearance = clearance
@@ -23,7 +23,10 @@ class ConditionalDataGrid(Dataset):
         self.out_path = out_path
         self.extract_id_dict = {}
         self.subsample = subsample
+        self.height_min_dif = height_min_dif
+        self.minimum_difs = torch.Tensor([self.grid_square_size/2,self.grid_square_size/2,self.height_min_dif])
         self.save_name = f"extract_id_dict_{subsample}_{self.sample_size}_{self.min_points}_{self.grid_square_size}.pt"
+        self.normalization = normalization
         if not preload:
             print(f"Recreating dataset, saving to: {self.out_path}")
             file_path_lists  = [[os.path.join(path,x) for x in os.listdir(path) if x.split('.')[-1]=='las'] for path in direcories_list]
@@ -49,7 +52,9 @@ class ConditionalDataGrid(Dataset):
                 for square_index,extract_list in enumerate(list(zip(*grids))):
                     
                     extract_list = [torch.from_numpy(x.astype(np.float32)) for x in extract_list if x.shape[0]>=self.min_points]
-                    extract_list = [x for x in extract_list if (x[:,2].max()-x[:,2].min())>0.5] #Filter out total flat ground
+                    #Check mins
+                    extract_list = [x for x in extract_list if ((x.max(dim=0)[0][:3]-x.min(dim=0)[0][:3] )>self.minimum_difs).all().item()]
+                    
                     if len(extract_list)<2:
                         continue
                     extract_id +=1 # Iterate after continue to not skip ints
@@ -58,7 +63,10 @@ class ConditionalDataGrid(Dataset):
                     elif self.subsample=='fps':
                         
                         extract_list = [ x[fps(x.cuda(),ratio = self.sample_size/x.shape[0])] if 0<self.sample_size/x.shape[0]<1 else x for x in extract_list]
-
+                    else:
+                        raise Exception("Invalid subsampling type")
+                    #Check mins again
+                    extract_list = [x for x in extract_list if ((x.max(dim=0)[0][:3]-x.min(dim=0)[0][:3] )>self.minimum_difs).all().item()]
                     for scan_index,extract in enumerate(extract_list):
                         
                         if not extract_id in self.extract_id_dict:
@@ -71,7 +79,7 @@ class ConditionalDataGrid(Dataset):
         else:
             self.extract_id_dict = torch.load(os.path.join(self.out_path,self.save_name))
         
-        #file_paths_list  = [os.path.join(self.out_path,x) for x in os.listdir(self.out_path) if x.split('.')[-1]=='npy']
+        
         self.combinations_list=[]
         for id,path_list in self.extract_id_dict.items():
             index_permutations = list(permutations(range(len(path_list)),2))
@@ -97,6 +105,12 @@ class ConditionalDataGrid(Dataset):
         view_cloud_plotly(cloud_1[:,:3],cloud_1[:,3:])
         view_cloud_plotly(cloud_2[:,:3],cloud_2[:,3:])
 
+    def test_nans(self):
+        for i in range(self.__len__()):
+            tensor_0, tensor_1 = self.__getitem__(i)
+            if (tensor_0.isnan().any() or tensor_1.isnan().any()).item():
+                raise Exception(f"Found nan at index {i}!")
+
     def __getitem__(self, idx):
         if torch.is_tensor(idx):
             idx = idx.tolist()
@@ -104,8 +118,13 @@ class ConditionalDataGrid(Dataset):
         relevant_tensors = self.extract_id_dict[combination_entry[0]]
         tensor_0 = relevant_tensors[combination_entry[1]]
         tensor_1 = relevant_tensors[combination_entry[2]]
-        if (tensor_0.isnan().any() or tensor_1.isnan().any()).item():
-            raise Exception("")
-        tensor_0[:,:3], tensor_1[:,:3] = co_min_max(tensor_0[:,:3],tensor_1[:,:3])
+        if self.normalization == 'min_max':
+            tensor_0[:,:3], tensor_1[:,:3] = co_min_max(tensor_0[:,:3],tensor_1[:,:3])
+        elif self.normalization == 'normalize':
+            concatenated = torch.cat((tensor_0[:,:3],tensor_1[:,:3]),dim=0)
+            concatenated = (concatenated-concatenated.mean(axis=0))/(concatenated.std()+eps)
+            tensor_0[:,:3], tensor_1[:,:3] = torch.split(concatenated,tensor_0.shape[0],dim=0)
+        else:
+            raise Exception('Invalid normalization type')
         
         return tensor_0,tensor_1

models/Exponential_matrix_flow.py
@@ -5,14 +5,15 @@ import torch
 from torch import nn
 from torch.distributions import constraints
 from torch.distributions.utils import _sum_rightmost
-
+import sys
 from pyro.distributions.conditional import ConditionalTransformModule
 from pyro.distributions.torch_transform import TransformModule
 from pyro.distributions.transforms.utils import clamp_preserve_gradients
 from pyro.distributions.util import copy_docs_from
-from nets import ConditionalDenseNN, DenseNN
+sys.path.append(".")
+from models.nets import ConditionalDenseNN, DenseNN
 
-eps = 1e-8
+eps = 1.2e-07
 
 class Exponential_matrix_coupling(TransformModule):
 

utils.py
@@ -17,7 +17,7 @@ from scipy.spatial.transform import Rotation
 from sklearn.neighbors import NearestNeighbors
 #Losses from original repo
 
-eps = 1e-8
+eps = 1.2e-07
 
 
 def loss_fun(z, z_ldetJ, prior_z, e, e_ldetJ, prior_e):
@@ -247,8 +247,9 @@ def save_las(pos,path,rgb=None,extra_feature=None,feature_name='Change'):
 def co_min_max(tensor_0,tensor_1):
     overall_max = torch.max(tensor_0[:,:3].max(axis=0)[0],tensor_1[:,:3].max(axis=0)[0])
     overall_min = torch.min(tensor_0[:,:3].min(axis=0)[0],tensor_1[:,:3].min(axis=0)[0])
-    tensor_0[:,:3] = (tensor_0[:,:3] - overall_min)/(overall_max-overall_min) + eps
-    tensor_1[:,:3] = (tensor_1[:,:3] - overall_min)/((overall_max-overall_min) + eps)
+    denominator = overall_max-overall_min  + eps
+    tensor_0[:,:3] = (tensor_0[:,:3] - overall_min)/denominator
+    tensor_1[:,:3] = (tensor_1[:,:3] - overall_min)/denominator
 
     if (tensor_0.isnan().any() or tensor_1.isnan().any()).item():
             raise Exception("")