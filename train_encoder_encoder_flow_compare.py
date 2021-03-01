7591fc29cc77c7313e878332086d9c4d170308a7
SamGalanakis <47306720+SamGalanakis@users.noreply.github.com>
2021-03-01 19:20:47 +0100

    cleanup train encoder encoder

--- train_encoder_encoder_flow_compare.py
+++ train_encoder_encoder_flow_compare.py
@@ -32,7 +32,7 @@ def initialize_encoder_models(config,device = 'cuda',mode='train'):
     flow_type = config['flow_type']
     permuter_type = config['permuter_type']
     hidden_dims = config['hidden_dims']
-    data_parallel = config['DataParallel']
+    data_parallel = config['data_parallel']
     parameters = []
 
     if flow_type == 'exponential_coupling':
@@ -56,9 +56,9 @@ def initialize_encoder_models(config,device = 'cuda',mode='train'):
     else:
         raise Exception(f'Invalid permuter type: {permuter_type}')
 
-    conditional_flow_layers = Conditional_flow_layers(flow,n_flow_layers,flow_input_dim,config['context_dim'],device,permuter,hidden_dims,config['batchnorm'])
+    conditional_flow_layers = Conditional_flow_layers(flow,config['n_flow_layers'],flow_input_dim,config['context_dim'],device,permuter,config['hidden_dims'],config['batchnorm'])
 
-    for transform in conditional_flow_layers.transforms:
+    for transform in conditional_flow_layers.transformations:
         if isinstance(transform,torch.nn.Module):
             if mode == 'train':
                 transform.train()
@@ -72,9 +72,9 @@ def initialize_encoder_models(config,device = 'cuda',mode='train'):
             wandb.watch(transform,log_freq=10)
 
     if config['encoder_type'] == 'pointnet2':
-        encoder = Pointnet2(feature_dim=input_dim-3,out_dim=context_dim)
+        encoder = Pointnet2(feature_dim=config["input_dim"]-3,out_dim=config["context_dim"])
     elif config['encoder_type'] == 'gcn':
-        encoder = GCNEncoder(in_dim= input_dim,out_channels=context_dim,k=20)
+        encoder = GCNEncoder(in_dim= config["input_dim"],out_channels=config["context_dim"],k=20)
     else:
         raise Exception('Invalid encoder type!')
     if data_parallel:
@@ -88,7 +88,7 @@ def initialize_encoder_models(config,device = 'cuda',mode='train'):
     parameters += encoder.parameters()
 
     if config['batchnorm_encodings']:
-        batchnorm_encoder = torch.nn.BatchNorm1d(context_dim)
+        batchnorm_encoder = torch.nn.BatchNorm1d(config["context_dim"])
         if data_parallel:
             batchnorm_encoder = nn.DataParallel(batchnorm_encoder).to(device)
         else:
@@ -113,83 +113,37 @@ def main(rank, world_size):
     config_path = r"config\config_conditional_encoder.yaml"
     wandb.init(project="flow_change",config = config_path)
     config = wandb.config
-    sample_size= config['sample_size'] 
-    n_flow_layers = config['n_flow_layers']
-    early_stop_margin = config['early_stop_margin']
-    hidden_dims = config['hidden_dims']
-    save_model_path = config['save_model_path']
-    count_bins =config['count_bins']
-    input_dim = config['input_dim']
-    batch_size = wandb.config['batch_size']
-    grid_square_size = config['grid_square_size']
-    clearance = config['clearance']
-    subsample = config['subsample']
-    patience = config['patience']
-    preload = config['preload']
-    min_points = config['min_points']
-    n_epochs = config['n_epochs']
-    context_dim = config['context_dim']
-    lr = config['lr']
-    num_workers = config['num_workers']
-    permuter_type = config['permuter_type']
-    scaler_type = config['scaler_type']
-    flow_type = config['flow_type']
-    batchnorm = config['batchnorm']
-    optimizer_type = config['optimizer_type']
-    batchnorm_encodings = config['batchnorm_encodings']
-    encoder_type = config['encoder_type']
-    weight_decay = config['weight_decay']
-    data_parallel = config['data_parallel']
-    data_loader = config['data_loader']
+   
     torch.backends.cudnn.benchmark = True
-    flow_input_dim = context_dim
+    flow_input_dim = config['context_dim']
 
     
     def collate_double_encode(batch):
         extract_0,extract_1 = list(zip(*batch))
 
-        combined_data_list = [Data(x=feature_assigner(x,input_dim),pos=x[:,:3]) for x in extract_0+extract_1]
+        combined_data_list = [Data(x=feature_assigner(x,config["input_dim"]),pos=x[:,:3]) for x in extract_0+extract_1]
         combined_batch = Batch.from_data_list(combined_data_list)
         return combined_batch
 
-  
-
-
-
-
-
-
     one_up_path = os.path.dirname(__file__)
     out_path = os.path.join(one_up_path,r"save/processed_dataset")
-    if data_loader == 'ConditionalDataGridSquare':
-        dataset=ConditionalDataGrid(dirs,out_path=out_path,preload=preload,subsample=subsample,sample_size=sample_size,min_points=min_points,grid_type='square')
-    elif data_loader == 'ConditionalDataGridCircle':
-        dataset=ConditionalDataGrid(dirs,out_path=out_path,preload=preload,subsample=subsample,sample_size=sample_size,min_points=min_points,grid_type='circle')
-    elif data_loader=='ShapeNet':
-        dataset = ShapeNetLoader(r'D:\data\ShapeNetCore.v2.PC15k\02691156\train',out_path=out_path,preload=preload,subsample=subsample,sample_size=sample_size)
+    if config['data_loader'] == 'ConditionalDataGridSquare':
+        dataset=ConditionalDataGrid(dirs,out_path=out_path,preload=config['preload'],subsample=config["subsample"],sample_size=config["sample_size"],min_points=config["min_points"],grid_type='square')
+    elif config['data_loader'] == 'ConditionalDataGridCircle':
+        dataset=ConditionalDataGrid(dirs,out_path=out_path,preload=config['preload'],subsample=config['subsample'],sample_size=config['sample_size'],min_points=config['min_points'],grid_type='circle')
+    elif config['data_loader']=='ShapeNet':
+        dataset = ShapeNetLoader(r'D:\data\ShapeNetCore.v2.PC15k\02691156\train',out_path=out_path,preload=config['preload'],subsample=config['subsample'],sample_size=config['sample_size'])
     else:
         raise Exception('Invalid dataloader type!')
-    shuffle=True
-    #SET PIN MEM TRUE
+
     
-    dataloader = DataLoader(dataset,shuffle=shuffle,batch_size=batch_size,num_workers=num_workers,collate_fn=collate_double_encode,pin_memory=True,prefetch_factor=2)
-
-
-
+    dataloader = DataLoader(dataset,shuffle=True,batch_size=config['batch_size'],num_workers=config["num_workers"],collate_fn=collate_double_encode,pin_memory=True,prefetch_factor=2)
 
     base_dist = dist.Normal(torch.zeros(flow_input_dim).to(device), torch.ones(flow_input_dim).to(device))
-
-
-    
-    
-
-
-
 
     models_dict = initialize_encoder_models(config,device,mode='train')
     
 
-    
     parameters = models_dict['parameters']
     encoder = models_dict['encoder']
     batchnorm_encoder = models_dict['batchnorm_encoder']
@@ -200,24 +154,24 @@ def main(rank, world_size):
 
     flow_dist = dist.ConditionalTransformedDistribution(base_dist, transformations)
     
-    if optimizer_type =='Adam':
-        optimizer = torch.optim.Adam(parameters, lr=lr,weight_decay=weight_decay) 
-    elif optimizer_type == 'Adamax':
-        optimizer = torch.optim.Adamax(parameters, lr=lr)
-    elif optimizer_type == 'AdamW':
-        optimizer = torch.optim.AdamW(parameters, lr=lr,weight_decay=weight_decay)
+    if config["optimizer_type"] =='Adam':
+        optimizer = torch.optim.Adam(parameters, lr=config["lr"],weight_decay=config["weight_decay"]) 
+    elif config["optimizer_type"] == 'Adamax':
+        optimizer = torch.optim.Adamax(parameters, lr=config["lr"])
+    elif config["optimizer_type"] == 'AdamW':
+        optimizer = torch.optim.AdamW(parameters, lr=config["lr"],weight_decay=config["weight_decay"])
     else:
         raise Exception('Invalid optimizer type!')
 
     
 
-    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer,factor=0.05,patience=patience,threshold=0.0001)
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer,factor=0.05,patience=config["patience"],threshold=0.0001)
     save_model_path = r'save/conditional_flow_compare'
     
 
 
     torch.autograd.set_detect_anomaly(False)
-    for epoch in range(n_epochs):
+    for epoch in range(config["n_epochs"]):
         print(f"Starting epoch: {epoch}")
         for batch_ind,batch in enumerate(tqdm(dataloader)):
             
@@ -225,7 +179,7 @@ def main(rank, world_size):
             optimizer.zero_grad()
             batch = batch.to(device)
             encodings = encoder(batch.to_data_list())
-            encoding_0, encoding_1 = torch.split(encodings,batch_size)
+            encoding_0, encoding_1 = torch.split(encodings,config["batch_size"])
     
          
   
