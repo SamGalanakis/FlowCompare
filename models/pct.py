af38f1c66faff87d4008d678cef6039a97cea884
SamGalanakis <47306720+SamGalanakis@users.noreply.github.com>
2021-03-28 13:33:59 +0200

    fix neighborhood embedder

--- models/pct.py
+++ models/pct.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from .pct_utils import sample_and_group 
+from pct_utils import sample_and_group 
 
 # Code sourced from : https://github.com/uyzhang/PCT_Pytorch
 
@@ -148,6 +148,8 @@ class NeighborhoodEmbedder(nn.Module):
     def __init__(self,input_dim,in_channels=[128,256],out_channels=[128,256]):
         super().__init__()
         self.input_dim = input_dim
+        self.out_channels = out_channels
+        self.in_channels = in_channels
         self.conv1 = nn.Conv1d(input_dim, 64, kernel_size=1, bias=False)
         self.conv2 = nn.Conv1d(64, 64, kernel_size=1, bias=False)
         self.bn1 = nn.BatchNorm1d(64)
@@ -160,18 +162,19 @@ class NeighborhoodEmbedder(nn.Module):
         xyz = x
         x = x.permute(0, 2, 1)
         
-        batch_size, _, _ = x.size()
+        batch_size, input_dim, n_points = x.size()
         # B, D, N
         x = F.relu(self.bn1(self.conv1(x)))
         # B, D, N
         x = F.relu(self.bn2(self.conv2(x)))
         x = x.permute(0, 2, 1)
-        new_xyz, new_feature = sample_and_group(npoint=512, radius=0.15, nsample=32, xyz=xyz, points=x)         
+        new_xyz, new_feature = sample_and_group(npoint=n_points, radius=0.15, nsample=32, xyz=xyz, points=x)         
         feature_0 = self.gather_local_0(new_feature)
         feature = feature_0.permute(0, 2, 1)
-        new_xyz, new_feature = sample_and_group(npoint=256, radius=0.2, nsample=32, xyz=new_xyz, points=feature) 
+        new_xyz, new_feature = sample_and_group(npoint=n_points, radius=0.2, nsample=32, xyz=new_xyz, points=feature) 
         feature_1 = self.gather_local_1(new_feature)
-        feature_1 = self.final_linear(feature_1)
+        feature_1 = self.final_linear(feature_1.reshape((batch_size,n_points,self.out_channels[1])))
+        
         return feature_1
 
 if __name__ == '__main__':
@@ -182,6 +185,6 @@ if __name__ == '__main__':
 
     argsz = args(0.5)
     model = NeighborhoodEmbedder(6).cuda()
-    points  = torch.randn((5,6,2000)).cuda()
+    points  = torch.randn((5,2000,6)).cuda()
     result = model(points)
     pass
