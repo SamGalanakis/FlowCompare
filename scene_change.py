500c6e33df3d84190b6dd0b952120ee2ac99b66f
Sam Galanakis <samme.galanakis6@gmail.com>
2021-07-02 00:14:43 +0200

    Scene changer sart

--- scene_change.py
+++ scene_change.py
@@ -7,28 +7,32 @@ from utils import (
 config_loader,
 extract_area,
 random_subsample,
-save_las,
-log_prob_to_color,
-circle_cover,
-co_unit_sphere
+save_las,get_all_voxel_centers,
 )
+from torch_cluster import grid_cluster
+from dataloaders.dataset_utils import registration_pipeline
 
 
 
-def change_map(cloud_0,cloud_1,clearance,config_path,model_path,change_func,out_path,batch_size,center = None,overlap=0,sample_size=2048):
+def change_map(cloud_0,cloud_1,model_path,change_func,out_path,batch_size,sample_size=2048,center = None):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    config = config_loader(config_path)
     cloud_0,cloud_1 = cloud_0.to(device),cloud_1.to(device)
-    models_dict = initialize_cross_flow(config,device,mode='train')
     checkpoint_dict = torch.load(model_path)
+    config = checkpoint_dict['config']
+    models_dict = initialize_cross_flow(config,device,mode='train')
     models_dict = load_cross_flow(checkpoint_dict,models_dict)
     if center == None:
         center = cloud_0[:,:2].mean()
-    cloud_0 = cloud_0[extract_area(cloud_0,center,clearance,shape='square'),...]
-    cloud_1 = cloud_1[extract_area(cloud_1,center,clearance,shape='square'),...]
-    grid = circle_cover(clearance,clearance,config['grid_square_size']/2,overlap=overlap,show=False)
+   
     
-    pairs = [[cloud_0[extract_area(cloud_0,point,radius),cloud_1[extract_area(cloud_1,point,radius)],...] for point in grid]
+    cloud_0,cloud_1 = registration_pipeline([cloud_0,cloud_1],0.05,0.07)
+    cloud_0 = cloud_0[extract_area(cloud_0,center,config['clearance'],shape='square'),...]
+    cloud_1 = cloud_1[extract_area(cloud_1,center,config['clearance'],shape='square'),...]
+    start,end = cloud_0.min(dim=0),cloud_0.max(dim=0)
+    
+    cluster_0 = grid_cluster(cloud_0[:,:3])
+    voxel_centers = get_all_voxel_centers(start,end,config['final_voxel_size'])
+    cluster_1 = grid_cluster(cloud_1[:,:3])
 
 
 
