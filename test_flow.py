f2ed0a1bd7e68eb91bc450f78b3fdadbacbd7252
Sam Galanakis <samme.galanakis6@gmail.com>
2021-08-14 13:45:22 +0200

    File cleanup

--- test_flow.py
+++ test_flow.py
@@ -2,7 +2,7 @@ from models.augmenter import Augment
 import torch
 from dataloaders import ChallengeDataset, AmsVoxelLoader, FullSceneLoader
 import os
-from utils import view_cloud_plotly,is_valid,min_max_norm,left_mad
+from utils import view_cloud_plotly,is_valid
 from visualize_change_map import visualize_change
 from tqdm import tqdm
 import models
@@ -39,7 +39,7 @@ class DatasetViewer:
         if not self.config['using_extra_context']:
             extra_context = None
         batch_1_0 = [voxel_0_large,voxel_1_small,extra_context]
-        batch_0_1 = [voxel_opposite_large,voxel_opposite_small,extra_context] #voxel_1_large,voxel_0_small
+        batch_0_1 = [voxel_opposite_large,voxel_opposite_small,extra_context] 
         
         batch_0_0 = [voxel_0_large_self,voxel_0_small_self,extra_context]
         batch_1_1 = [voxel_1_large_self,voxel_1_small_self,extra_context]
@@ -51,7 +51,6 @@ class DatasetViewer:
             batch_0_0, self.model_dict, config)
         assert is_valid(log_prob_1_0)
         change_1_0 = log_prob_to_change(log_prob_1_0,log_prob_0_0,multiple=multiple,hard_cutoff=hard_cutoff)
-        #change_1_0 = left_mad(log_prob_1_0.squeeze(),log_prob_0_0.squeeze(),cutoff = hard_cutoff)
         
         assert is_valid(change_1_0)
 
@@ -65,7 +64,7 @@ class DatasetViewer:
         _,log_prob_1_1,_ = inner_loop(
             batch_1_1, self.model_dict, config)
         change_0_1 = log_prob_to_change(log_prob_0_1,log_prob_1_1,multiple=multiple,hard_cutoff=hard_cutoff)
-        #change_0_1 = left_mad(log_prob_0_1.squeeze(),log_prob_1_1.squeeze(),cutoff = hard_cutoff)
+        
         assert is_valid(log_prob_0_1)
 
         sample_points_given_0 = make_sample(
@@ -277,120 +276,13 @@ def log_prob_to_change(log_prob_1_given_0, log_prob_0_given_0,multiple,hard_cuto
     return log_prob_1_given_0
 
 
-def change_to_color(change,mode='solid'):
-    color = torch.zeros(change.shape+(3,))
-    if mode == 'solid':
-        change[change>0] = 1.
-
-    return color
-
-def dataset_view(dataset, index, multiple=3., gen_std=0.6, show=False,save=True):
-    with torch.no_grad():
-        sample_distrib = models.Normal(torch.zeros(1), torch.ones(
-            1)*gen_std, shape=(2000, config['latent_dim'])).to(device)
-        return_dict, label = dataset[index]
-        print('starting calc ')
-        extract_0, extract_1 = return_dict['cloud_0'], return_dict['cloud_1']
-        vars = ['gen_given_0', 'gen_given_1', 'change_1_given_0',
-                'change_0_given_1', 'voxel_0', 'voxel_1']
-        procesed_dict = {x: [] for x in vars}
-
-
-        def get_log_prob(voxel,context_voxel):
-            voxel,context_voxel,inverse = dataset.last_processing(voxel,context_voxel)
-            extra_context = inverse['mean'][2].reshape(-1,1) if config['using_extra_context'] else None
-            
-            log_prob_1_given_0 = calc_change(
-                [context_voxel.unsqueeze(0), voxel.unsqueeze(0),extra_context], model_dict, config).cpu()
-            return voxel,context_voxel,inverse,log_prob_1_given_0
-
-        def to_dev(x, device):
-            try:
-                return x.to(device)
-            except:
-                return x
-        for key, val in return_dict['voxels'].items():
-
-            context_for_1,voxel_1,context_0_0,context_for_0,voxel_0,context_1_1,z_voxel_center = [
-                to_dev(x, device) for x in val]
-            print(f'Vox center {z_voxel_center}')
-            extra_context  = z_voxel_center.reshape((1,-1)) if config['using_extra_context'] else None
-
-
-
-            _,context_for_1,inverse_1,log_prob_1_given_0 = get_log_prob(voxel_1,context_for_1)
-            _,_,_,log_prob_0_given_0 = get_log_prob(voxel_0,context_0_0)
-            _,context_for_0,inverse_0,log_prob_0_given_1 = get_log_prob(voxel_0,context_for_0)
-            _,_,_,log_prob_1_given_1 = get_log_prob(voxel_1,context_1_1)
-
-            if save:
-                plt.hist(log_prob_0_given_0.numpy(),label='00')
-                plt.hist(log_prob_0_given_1.numpy(),label='01')
-                plt.legend(loc="upper left")
-                plt.savefig(f'save/examples/{index}_{key}_hist_00and01.png')
-                plt.clf()
-                plt.close()
-
-
-            gen_given_0 = make_sample(1024, context_for_1.unsqueeze(
-                0), model_dict, config, sample_distrib=sample_distrib,extra_context=extra_context)
-            gen_given_1 = make_sample(1024, context_for_0.unsqueeze(
-                0), model_dict, config, sample_distrib=sample_distrib,extra_context=extra_context)
-            gen_given_0[:, :3] = inverse_map(gen_given_0[:, :3],inverse_1)
-            gen_given_1[:, :3] = inverse_map(gen_given_1[:, :3],inverse_0)
-            procesed_dict['gen_given_0'].append(gen_given_0.cpu())
-            procesed_dict['gen_given_1'].append(gen_given_1.cpu())
-
-            procesed_dict['change_1_given_0'].append(log_prob_to_change(
-                log_prob_1_given_0, log_prob_0_given_0, multiple=multiple).cpu())
-            procesed_dict['change_0_given_1'].append(log_prob_to_change(
-                log_prob_0_given_1, log_prob_1_given_1, multiple=multiple).cpu())
-
-            if save:
-                view_cloud_plotly(gen_given_0[:,:3],gen_given_0[:,3:],show=False).write_html(f'save/examples/{index}_{key}_gen_given_0.html')
-                view_cloud_plotly(voxel_0[:,:3],voxel_0[:,3:],show=False).write_html(f'save/examples/{index}_{key}_voxel_0.html')
-                view_cloud_plotly(gen_given_1[:,:3],gen_given_1[:,3:],show=False).write_html(f'save/examples/{index}_{key}_gen_given_1.html')
-                view_cloud_plotly(voxel_1[:,:3],voxel_1[:,3:],show=False).write_html(f'save/examples/{index}_{key}_voxel_1.html')
-          
-            # voxel_0[:, :3] =  inverse_map(voxel_0[:, :3],inverse_1).cpu()
-            # voxel_1[:, :3] = inverse_map(voxel_1[:, :3],inverse_0).cpu()
-            procesed_dict['voxel_0'].append(voxel_0.cpu())
-            procesed_dict['voxel_1'].append(voxel_1.cpu())
-
-        procesed_dict = {key: torch.cat(val, dim=0)
-                         for key, val in procesed_dict.items()}
-  
-        fig_gen_given_0 = view_cloud_plotly(
-            procesed_dict['gen_given_0'][:, :3], procesed_dict['gen_given_0'][:, 3:], show=show, title='Gen given 0')
-        fig_gen_given_1 = view_cloud_plotly(
-            procesed_dict['gen_given_1'][:, :3], procesed_dict['gen_given_1'][:, 3:], show=show, title='Gen given 1')
-        fig_0_given_1 = view_cloud_plotly(procesed_dict['voxel_0'][:, :3], procesed_dict['change_0_given_1'],
-                                        colorscale='Bluered', show_scale=True, show=show, title='Extract 0 given 1')
-        fig_1_given_0 = view_cloud_plotly(procesed_dict['voxel_1'][:, :3], procesed_dict['change_1_given_0'],
-                                        colorscale='Bluered', show_scale=True, show=show, title='Extract 1 given 0')
-        fig_0 = view_cloud_plotly(
-            extract_0[:, :3], extract_0[:, 3:], show=show, title='Extract 0', point_size=5)
-        fig_1 = view_cloud_plotly(
-            extract_1[:, :3], extract_1[:, 3:], show=show, title='Extract 1', point_size=5)
-        return fig_0, fig_1, fig_1_given_0, fig_0_given_1, fig_gen_given_1, fig_gen_given_0
-
-
-
 
 
 if __name__ == '__main__':
     
-    #name = load_path.split('/')[-1].split('_')[0]
-    #dataset_out = f"save/processed_dataset/{name}_{mode}_probs_dataset.pt"
-    #create_dataset(dataset,model_dict,dataset_out = dataset_out)
-    # score_on_test(dataset,model_dict,n_bins=12)
+
     load_path = 'save/conditional_flow_compare/swept-energy-2784_e1_b500_model_dict.pt'
 
-
-
-    
-
-    #load_path = 'save/conditional_flow_compare/valiant-lion-2798_e1_b17000_model_dict.pt'
     save_dict = torch.load(load_path, map_location=device)
     config = save_dict['config']
     model_dict = initialize_flow(config, device, mode='test')
@@ -399,50 +291,8 @@ if __name__ == '__main__':
     dataset_viewer = DatasetViewer(model_dict,config,mode=mode)
 
     
-    index_for_figs_list = [19857,63146,3092,152672,20579,133479,101532,182617,76605,46078,76115,49989,24434,76034]
-    save_path = 'save/examples/figs_thesis'
+    #index_for_figs_list = [19857,63146,3092,152672,20579,133479,101532,182617,76605,46078,76115,49989,24434,76034]
+  
     
-    for index in index_for_figs_list:
-        for hard_cutoff in [0,1.5,5,9]:
-        
-            out_list = dataset_viewer.view_index(index, multiple=3.3, gen_std=0.6,hard_cutoff=hard_cutoff,point_size=7)
-            out_names = ["fig_0","fig_1","fig_gen_given_0","fig_gen_given_1","combined_fig","fig_0_given_1","fig_1_given_0"]
-            for out_name,fig in zip(out_names,out_list[:-1]):
-                out_path = os.path.join(save_path,f'{index}_{hard_cutoff}_{out_name}.html')
-                fig.write_html(out_path)
-
-
-
-    # nats,log_probs_list = evaluate_on_test(model_dict,config,generate_samples=False)
-    # values,indices = torch.sort(torch.tensor(log_probs_list),descending=False) 
-    # torch.save({'values':values,'indices':indices},f'save/most_changed/{os.path.basename(load_path)}')
-
-
-    # textfile = open("a_file.txt", "w")
-    # for val,index in zip(values.tolist() ,indices.tolist()):
-    #     textfile.write(f"{str(val)},{str(index)}" + "\n")
-    # textfile.close()
-    # one_up_path = os.path.dirname(__file__)
-    # out_path = os.path.join(one_up_path, r"save/processed_dataset")
-
-    # dataset = AmsVoxelLoader(config['directory_path_train'],config['directory_path_test'], out_path='save/processed_dataset', preload=True,
-    #         n_samples = config['sample_size'],final_voxel_size = config['final_voxel_size'],device=device,
-    #         n_samples_context = config['n_samples_context'], context_voxel_size = config['context_voxel_size'],mode='test')
-
-
-    #visualize_index(1,config,multiple=3.3)
-    # preload = True
-    # csv_path = 'save/challenge_data/Shrec_change_detection_dataset_public/new_final.csv'
-    # dirs = ['save/challenge_data/Shrec_change_detection_dataset_public/' +
-    #         year for year in ["2016", "2020"]]
-    # dataset = ChallengeDataset(csv_path, dirs, out_path, n_samples=config['sample_size'], preload=preload, device=device, final_voxel_size=config[
-    #                         'final_voxel_size'], n_samples_context=config['n_samples_context'], context_voxel_size=config['context_voxel_size'])
-    
-
-    
-    
-    #dataset_view(dataset,0,multiple = 3.,show=False)
-    # # pass
-    # for x in range(0,12):
-    #    dataset_view(dataset,x,multiple = 3.,gen_std=0.6,save=True)
+ 
     visualize_change(lambda index, multiple, gen_std, hard_cutoff,point_size: dataset_viewer.view_index(index, multiple=multiple, gen_std=gen_std,hard_cutoff=hard_cutoff,point_size=point_size), range(len(dataset_viewer.dataset)))
